ccf124b68fdd08455f6298e52eb3f8c1dfec47e7
persimon-c <simone.dagondon1027@gmail.com>
2026-06-10 07:10:47 +0800

    change hyprland layout settings

--- dot_config/hypr/modules/layout.lua
+++ dot_config/hypr/modules/layout.lua
@@ -4,8 +4,8 @@
 hl.config({
     general = {
         gaps_in = 5,
-        gaps_out = 10,
-        border_size = 2,
+        gaps_out = 17,
+        border_size = 1,
         layout = "dwindle",
  
         -- NOTE: active/inactive border colors are set by colors.lua (matugen output or fallback).
